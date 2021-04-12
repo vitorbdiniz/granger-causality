0d60c310decebaaba7abc6d330c66006e1f48cc4
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2021-04-12 15:41:04 -0300

    conserta erros de calculo nas metricas

--- characteristics.py
+++ characteristics.py
@@ -5,64 +5,60 @@ import datetime as dt
 
 from factors import nefin_risk_free, nefin_single_factor
 
-import padding as pad
+import util,padding as pad
 
 def extract_characteristics(alfas, fis, verbose=0):
     characteristics = { 
         FI: pd.DataFrame({
-            'sharpe' : sharpe_index(fis[FI]['variacao'], verbose=verbose),
-            'treynor' : treynor_index(fis[FI]['variacao'], alfas[FI]['beta_MKT'], verbose=verbose),
-            'lifetime' : get_lifetime(fis[FI]['variacao'], verbose=verbose),
-            'information' : information_ratio(fis[FI]['variacao'], verbose=verbose),
-            'vol' : volatility(fis[FI]['variacao'], method="std", verbose=verbose),
-            'vol_dsd' : volatility(fis[FI]['variacao'], method="dsd", verbose=verbose)
-            },
-            index = fis[FI].index) 
+            'sharpe'             : sharpe_index(fis[FI]['variacao'].dropna(), portfolio=FI, verbose=verbose),
+            'treynor'            : treynor_index(fis[FI]['variacao'].dropna(),alfas[FI]['beta_MKT'].dropna(), portfolio=FI, verbose=verbose),
+            'lifetime'           : get_lifetime(fis[FI]['variacao'].dropna() , portfolio=FI,verbose=verbose),
+            'information-ratio'  : information_ratio(fis[FI]['variacao'].dropna(), portfolio=FI,verbose=verbose),
+            'standard_deviation' : volatility(fis[FI]['variacao'].dropna(), portfolio=FI,method="std", verbose=verbose),
+            'downside_deviation' : volatility(fis[FI]['variacao'].dropna(), portfolio=FI,method="dsd", verbose=verbose)
+            }, index = util.date_range(dt.date(2000,1,1), dt.date.today()) ).dropna(how="all")
         for FI in fis.keys() 
     }
-    return pd.DataFrame(characteristics, index = characteristics['sharpe'])
+    return characteristics
 
-def capm_risk_premium(returns, verbose=0):
+def capm_risk_premium(returns,verbose=0):
     pad.verbose("Calculando Prêmio pelo risco de Mercado", level=2, verbose=verbose)
     return (returns - nefin_risk_free()).dropna()
 
-def sharpe_index(returns, verbose=0):
-    pad.verbose("Calculando Índice de Sharpe", level=2, verbose=verbose)
+def sharpe_index(returns, portfolio=None,verbose=0):
+    pad.verbose(f"Calculando Índice de Sharpe de {portfolio}", level=2, verbose=verbose)
     return ( capm_risk_premium(returns) / volatility(returns) ).dropna()
 
-def treynor_index(returns, betas, verbose=0):
-    pad.verbose("Calculando Índice de Treynor", level=2, verbose=verbose)
+def treynor_index(returns, betas, portfolio=None,verbose=0):
+    pad.verbose(f"Calculando Índice de Treynor de {portfolio}", level=2, verbose=verbose)
     return ( capm_risk_premium(returns) / betas ).dropna()
 
-def get_lifetime(returns, verbose=0):
-    pad.verbose("Calculando Tempo de vida", level=2, verbose=verbose)
-    return range(1, (returns.index[ returns.shape[0] ] - returns.index[0]).days +1)  
+def get_lifetime(returns, portfolio=None, verbose=0):
+    pad.verbose(f"Calculando Tempo de vida de {portfolio}", level=2, verbose=verbose)
+    return pd.Series( range(1, returns.shape[0]+1) , index = returns.index)
 
 def leverage():
     return #TODO
 
 
 def downside_deviation(returns):
-    s = 0
-    c = 0
-    avg = st.mean(returns)
-    for r in returns:
-        if r < avg:
-            c += 1
-            s += (r - avg)**2
-    dsd = s/c
-    return dsd
+    avg = st.mean(returns.values)
+    down_values = returns[returns < avg]
+    if len(down_values) < 2:
+        return None
+    return st.stdev(down_values, xbar=avg)
 
 
-
-
-def volatility(returns, method="std", verbose=0):
+def volatility(returns, method="std", portfolio=None, verbose=0):
     """
         Calculate volatility by the sample's standard deviation(std) or by its downside deviation (dsd)
         method: {'std', 'dsd'}
         returns: float
     """
-    pad.verbose(f"Calculando Volatilidade: método {method}", level=2, verbose=verbose)
+    pad.verbose(f"Calculando Volatilidade de {portfolio}: método {method}", level=2, verbose=verbose)
+    if len(returns) < 2:
+        return None
+
     if method == "std":
         vol = pd.Series( [(returns.iloc[0:i]).std() for i in range(1, returns.shape[0]+1)], index = returns.index )
     elif method == "dsd":
@@ -70,12 +66,15 @@ def volatility(returns, method="std", verbose=0):
 
     return vol
 
-def tracking_error(returns, MKT, verbose=0):
-    pad.verbose("Calculando Tracking Error", level=2, verbose=verbose)
-    return volatility( (returns-MKT).dropna() )
-    
-def information_ratio(returns, verbose=0):
-    pad.verbose("Calculando Information Ratio", level=2, verbose=verbose)
+
+def information_ratio(returns, portfolio=None, verbose=0):
+    pad.verbose(f"Calculando Information Ratio de de {portfolio}", level=2, verbose=verbose)
+
     MKT = nefin_single_factor(factor="Market")
-    return ((returns - MKT).dropna() / tracking_error(returns, MKT)).dropna()
+    premium = (returns - MKT).dropna()
+    fund_tracking_error = volatility( (returns-MKT).dropna() )
+
+    IR = premium / fund_tracking_error
+
+    return IR
 
