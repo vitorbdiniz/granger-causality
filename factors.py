5969448dc38974b9e5617d609c12873bccc55e28
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2021-04-12 15:39:15 -0300

    Conserta nefin_single_factor

--- factors.py
+++ factors.py
@@ -16,24 +16,20 @@ concat_date = lambda date : pd.DatetimeIndex([dt.date(date[0].iloc[i], date[1].i
 
 def nefin_factors(verbose=0):
     pad.verbose("Downloading Nefin Factors", level=3, verbose=verbose)
-    mkt = pd.read_excel("http://nefin.com.br/Risk%20Factors/Market_Factor.xls") 
-    hml = pd.read_excel("http://nefin.com.br/Risk%20Factors/HML_Factor.xls")    
-    smb = pd.read_excel("http://nefin.com.br/Risk%20Factors/SMB_Factor.xls")    
-    wml = pd.read_excel("http://nefin.com.br/Risk%20Factors/WML_Factor.xls")    
-    iml = pd.read_excel("http://nefin.com.br/Risk%20Factors/IML_Factor.xls")    
-
-    risk_factors = pd.DataFrame({"year":mkt["year"], "month":mkt["month"], "day":mkt["day"], "MKT" : mkt["Rm_minus_Rf"], "HML" : hml["HML"], "SMB" : smb["SMB"],"WML" : wml["WML"],"IML" : iml["IML"]})
-    risk_factors.index = concat_date([risk_factors["year"], risk_factors["month"], risk_factors['day']])
-    risk_factors = risk_factors.drop(columns=["year", "month", "day"])
+    risk_factors = {factor : nefin_single_factor(factor=factor) for factor in ['Market', 'HML', 'SMB', 'WML', 'IML']}
 
     return risk_factors
 
 def nefin_single_factor(factor="Market"):
+    """
+        factor: {'Market', 'HML', 'SMB', 'WML', 'IML'}
+
+        return: {pandas.Series}
+    """
     F = pd.read_excel(f"http://nefin.com.br/Risk%20Factors/{factor}_Factor.xls")
     index = concat_date([ F['year'], F['month'], F['day'] ])
-    values = F[ F.columns[-1] ]
-
-    return pd.Series(values, index=index)
+    serie = pd.Series(list(F[ F.columns[-1] ]), index=index)
+    return serie
 
 def nefin_risk_free():
     nefin_Rf = pd.read_excel('http://nefin.com.br/Risk%20Factors/Risk_Free.xls')
