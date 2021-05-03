ebfb7884b76c631958f2178c239d07a407e8186f
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2021-05-03 10:53:37 -0300

    Atualiza calculo das caracteristicas

--- funds/characteristics.py
+++ funds/characteristics.py
@@ -4,72 +4,104 @@ import statistics as st
 import datetime as dt
 
 from factors.factors import nefin_risk_free, nefin_single_factor
-
 from util import util,padding as pad
 
-def extract_characteristics(alfas, fis, window=None, dropna="all", verbose=0):
+def extract_characteristics(alphas:dict, fias_characteristics = None, freq = 'M', window = None, dropna="all", verbose=0):
     '''
-        Calcula caracteristicas do dicionário de portfolios `fis` passado dentro de uma janela móvel `window`de tempo.
+        Calcula características do dict `fias_characteristics` que contém informações dos fundos.
+        alphas: {dict} -> contendo o alfa e os betas em dict de DataFrames;
+        fias_characteristics: {dict, None} -> se None, então a busca é automática
+        window: {'M', 'Q', 'Y'}
+
+        return: {dict} -> dict de DataFrames de fundos. Cada DF é uma característica específica
     '''
-    characteristics = dict()
-    for FI in fis.keys():
-        pad.verbose(f"{FI}. Calculando características", level=5, verbose=verbose)
-        characteristics[FI] = pd.DataFrame({
-            'sharpe'             : sharpe_index(fis[FI]['variacao'].dropna(), window=window, portfolio=FI, verbose=verbose),
-            'treynor'            : treynor_index(fis[FI]['variacao'].dropna(),alfas[FI]['beta_Market'].dropna(), window=window, portfolio=FI, verbose=verbose),
-            'lifetime'           : get_lifetime(fis[FI]['variacao'].dropna(), portfolio=FI,verbose=verbose),
-            'information-ratio'  : information_ratio(fis[FI]['variacao'].dropna(), window=window, portfolio=FI,verbose=verbose),
-            'standard_deviation' : volatility(fis[FI]['variacao'].dropna(), window=window, portfolio=FI,method="std", verbose=verbose),
-            'downside_deviation' : volatility(fis[FI]['variacao'].dropna(), window=window, portfolio=FI,method="dsd", verbose=verbose),
-            'variacao'           : get_return(fis[FI]['variacao'], window=window, return_type = pd.Series),
-            'equity'             : fis[FI]['patrimonio_liquido'],
-            'captacao_liquida'   : captacao_liquida_acumulada(fis[FI]['captacao'] - fis[FI]['resgate'], window=window, portfolio=FI, verbose=verbose),
-            'cotistas'           : fis[FI]['cotistas']
-            }, index = util.date_range(dt.date(2000,1,1), dt.date.today()) ).dropna(how=dropna)
+    if fias_characteristics is None:
+        fias_characteristics = get_characts(freq)
+
+    result = {
+        'variation'          : get_return_df(fias_characteristics['fis_acc'], window=window),
+        'sharpe'             : get_sharpe_ratio_df(fias_characteristics['fis_acc'], freq=freq, window=window, verbose=verbose),
+        'treynor'            : get_treynor_ratio_df(fias_characteristics['fis_acc'], alphas, freq=freq, window=window, verbose=verbose),
+        'lifetime'           : fias_characteristics['lifetime'],
+        'information_ratio'  : information_ratio_df(fias_characteristics['fis_acc'] , freq=freq, window=window, verbose=verbose),
+        'standard_deviation' : volatility_df(fias_characteristics['fis_acc'], method='std', window=window, verbose=verbose),
+        'downside_deviation' : volatility_df(fias_characteristics['fis_acc'], method='dsd', window=window, verbose=verbose),
+        'equity'             : fias_characteristics['PL'],
+        'captacao'           : fias_characteristics['capt'],
+        'captacao_liquida'   : fias_characteristics['capt_liq'],
+        'cotistas'           : fias_characteristics['cotistas']
+    }
+    return result
+
+def get_characts(freq='M'):
+    freq_dic = {'M':'month', 'Q':'quarter', 'Y':'year', 'D':'day'}
+    names = ['fis_acc', 'capt', 'capt_liq', 'cotistas', 'PL', 'resgate', 'resgate_IR', 'resgate_total', 'lifetime']
+    characteristics = {n : util.df_datetimeindex(pd.read_csv(f'./data/economatica/{freq_dic[freq]}/{n}_economatica_{freq_dic[freq]}.csv', index_col=0)) for n in names}
     return characteristics
 
-def captacao_liquida_acumulada(captacao_liquida, window=None, portfolio='', verbose=0):
-    s = 0
-    acc = []
-    pad.verbose(f"- {portfolio}. Captação Líquida", level=3, verbose=verbose)
-    for cap in captacao_liquida.values:
-        s += cap if pd.notna(cap) else 0
-        acc += [s]
-    return pd.Series(acc, captacao_liquida.index)
+def capm_risk_premium(returns, Rf=None, freq = None, window=None):
+    if Rf is None:
+        Rf = nefin_risk_free(freq=freq)
+    Rp = (returns - nefin_risk_free()).dropna()
+    risk_premium = get_return(Rp, window=window)
+    return risk_premium
 
-
-def capm_risk_premium(returns,cumulative = False, window=None, verbose=0):
-    pad.verbose("- Prêmio pelo risco de Mercado", level=2, verbose=verbose)
-    mkt = (returns - nefin_risk_free()).dropna()
-    if cumulative:
-        mkt = get_return(mkt, window=window, return_type = pd.Series)
-    return mkt
-
-def sharpe_index(returns, window=None, portfolio=None,verbose=0):
-    pad.verbose(f"- {portfolio}. Índice de Sharpe", level=2, verbose=verbose)
-    risk_premium = capm_risk_premium(returns, cumulative = True, window=window, verbose=0)
+'''
+    SHARPE
+'''
+def get_sharpe_ratio(returns:pd.Series, Rf=None, freq=None, window=None):
+    risk_premium = capm_risk_premium(returns, Rf=Rf, freq=freq, window=window)
     vol = volatility(returns,window=window)
     sharpe = ( risk_premium / vol ).dropna()
     return sharpe
 
-def treynor_index(returns, betas, window=None, portfolio=None,verbose=0):
-    pad.verbose(f"- {portfolio}. Índice de Treynor", level=2, verbose=verbose)
-    risk_premium = capm_risk_premium(returns, cumulative = True,window=window, verbose=0)
-    treynor = (risk_premium / betas).dropna()
+def get_sharpe_ratio_df(returns:pd.DataFrame, freq=freq, window=None, verbose=0):
+    result = pd.DataFrame(columns = returns.columns, index = returns.index)
+    Rf = nefin_risk_free(freq=freq)
+    for i in returns.shape[1]:
+        portfolio = returns.columns[i]
+        pad.verbose(f'{i}. Índice de Sharpe --- frequência {freq} --- janela {window} --- Faltam {returns.shape[1]-i}', level=5, verbose=verbose)
+        result[portfolio] = get_sharpe_ratio(returns[portfolio], Rf=Rf, freq=freq, window=window)
+    return result.dropna(how='all')
+
+'''
+    TREYNOR
+'''
+
+def get_treynor_ratio(returns:pd.Series, betas:pd.Series, Rf:pd.Series=None, freq=None, window=None):
+    risk_premium = capm_risk_premium(returns, Rf=Rf, freq=freq, window=window)
+    treynor = ( risk_premium / betas ).dropna()
     return treynor
 
-def get_lifetime(returns, portfolio=None, verbose=0):
-    pad.verbose(f"{portfolio}. Calculando Tempo de vida", level=2, verbose=verbose)
-    return pd.Series( range(1, returns.shape[0]+1) , index = returns.index)
 
-def volatility(returns, method="std", window=None, portfolio=None, verbose=0):
+def get_treynor_ratio_df(returns:pd.DataFrame, alphas:dict, freq=None, window=None, verbose=0):
+    result = pd.DataFrame(columns = returns.columns, index = returns.index)
+    Rf = nefin_risk_free(freq=freq)
+    for i in returns.shape[1]:
+        portfolio = returns.columns[i]
+        pad.verbose(f'{i}. Índice de Treynor --- frequência {freq} --- janela {window} --- Faltam {returns.shape[1]-i}', level=5, verbose=verbose)
+        result[portfolio] = get_treynor_ratio(returns[portfolio], alphas[portfolio]['beta_Market'], Rf=Rf, freq=freq, window=window)
+    return result.dropna(how='all')
+
+'''
+    VOLATILITY
+'''
+ 
+def volatility_df(returns:pd.DataFrame, method="std", window=None, verbose=0):
+    result = pd.DataFrame(columns = returns.columns, index = returns.index)
+    for i in returns.shape[1]:
+        portfolio = returns.columns[i]
+        pad.verbose(f'{i}. Volatilidade --- método {method} --- frequência {freq} --- janela {window} --- Faltam {returns.shape[1]-i}', level=5, verbose=verbose)
+        result[portfolio] = volatility(returns[portfolio], method=method, window=window)
+    return result.dropna(how='all')
+
+def volatility(returns:pd.Series, method="std", window=None):
     """
         Calculate volatility by the sample's standard deviation(std) or by its downside deviation (dsd)
         method: {'std', 'dsd'}
         cumulative: se cumulative < 0, então é calculada a volatilidade acumulada, senão, é calculada dentro da janela de `cumulative` meses
         returns: pd.Series(dtype=np.float)
     """
-    pad.verbose(f"- {portfolio}. Volatilidade: método {method}", level=2, verbose=verbose)
     if len(returns) < 2:
         return None
 
@@ -102,17 +134,29 @@ def downside_deviation(returns):
     return st.stdev(down_values, xbar=avg)
 
 
-def information_ratio(returns, window=None, portfolio=None, verbose=0):
-    pad.verbose(f"- {portfolio}. Information Ratio", level=2, verbose=verbose)
+'''
+    INFORMATION RATIO
+'''
+def information_ratio_df(returns:pd.DataFrame, freq=None, window=None, verbose=0):
+    result = pd.DataFrame(columns = returns.columns, index = returns.index)
+    MKT = nefin_single_factor(factor="Market", freq=freq)
+    for i in returns.shape[1]:
+        portfolio = returns.columns[i]
+        pad.verbose(f'{i}. Information-Ratio --- janela {window} --- Faltam {returns.shape[1]-i}', level=5, verbose=verbose)
+        result[portfolio] = information_ratio(returns[portfolio], MKT, window=window)
+    return result.dropna(how='all')
 
-    MKT = nefin_single_factor(factor="Market")
+def information_ratio(returns:pd.Series, mkt_returns=None, window=None):
+    difference = (returns - MKT).dropna()
+    premium = get_return(difference, window=window)
+    tracking_error = volatility( difference, method='std', window=window )
+    IR = premium / tracking_error
+    return IR.dropna()
 
-    premium = get_return((returns - MKT).dropna(), window=window, return_type=pd.Series)
-    fund_tracking_error = volatility( (returns-MKT).dropna(), window=window )
 
-    IR = premium / fund_tracking_error
-
-    return IR
+'''
+    RETORNOS ACUMULADOS POR JANELA
+'''
 
 def get_return(retornos, window=None, return_type = pd.Series):
     '''
@@ -124,3 +168,12 @@ def get_return(retornos, window=None, return_type = pd.Series):
         retornos_acc = [util.cumulative_return(retornos[i-window:i], return_type=list)[-1] for i in range(window, len(retornos)+1)]
         retornos_acc = pd.Series(retornos_acc, index=retornos.index[window-1:len(retornos)])
     return retornos_acc
+
+def get_return_df(returns:pd.DataFrame, window=None):
+    for i in returns.shape[1]:
+        portfolio = returns.columns[i]
+        pad.verbose(f'{i}. Retorno acumulado --- janela {window} --- Faltam {returns.shape[1]-i}', level=5, verbose=verbose)
+        result[portfolio] = get_return(returns[portfolio], window=window)
+    return result.dropna(how='all')
+
+
